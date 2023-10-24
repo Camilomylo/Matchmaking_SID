ffaabcc26c166179333d2ec61885c608139cde97
Camilomylo <juan.piza@upb.edu.co>
2023-10-24 08:23:34 -0500

    mis codigos

---
+++ Assets/Camilo P/Notificaciones/Notificaciones/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Data;
+using System.Runtime.CompilerServices;
+
+private void btnConfigurarNotificacion_Click(object sender, EvantArgs e);
+
+private void MostrarNotificacion(string mensaje)
+{
+    MessageBox.Show(mensaje, "Notificación", MessageBoxButton.OK, MessageBoxIccon.Information);
+}
+{
+    DataSetDateTime fechaNotificacion = dateTimePickerFecha.Value;
+    string mensajeNotificacion = textBoxMensaje.Text;
+
+    Timer timer = new Timer();
+    timer.Interval = (int)(fechaNotificacion - DateTime.Now).TotalMilliseconds;
+    timer.Tick += (string, args) =>
+    {
+        MostrarNotificacion(mensajeNotificacion);
+        timer.Stop();
+    }
+    timer.Start();
+}
+
+
---
+++ Assets/Camilo P/amigosonline/amigosonline/Program.cs
@@ -0,0 +1,35 @@
+﻿
+public class ChatHub : Hub
+public class UsuarioService
+{ 
+ {
+    private IHubContext<ChatHub> _hubContext;
+
+    public UsuarioService(IHubContext<ChatHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public async Task UsuarioIniciaSesion(string userId)
+    {
+        await _hubContext.Clients.All.SendAsync("UsuarioConectado", userId);
+    }
+
+    public async Task UsuarioCierraSesion(string userId)
+    {
+        await _hubContext.Clients.All.SendAsync("UsuarioDesconectado", userId);
+    }
+ }
+
+
+    public async Task UsuarioConectado(string userId)
+    {
+        await Clients.All.SendAsync("UsuarioConectado", userId);
+    }
+
+    public async Task UsuarioDesconectado(string userId)
+    {
+        await Clients.All.SendAsync("UsuarioDesconectado", userId);
+    }
+}
+
